edc8bfa3538c53f7e0566863290ff5f1f907b785
Arpit Mittal <75568572+arpit-mittal8@users.noreply.github.com>
2023-06-06 08:22:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+# 52:56 youtube
+import pandas as pd
+from flask import Flask, render_template, request
+#import requests
+import pickle
+import numpy as np
+#import sklearn
+#from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__)
+data = pd.read_csv('cleaned_data.csv')
+pipe = pickle.load(open("RidgeModel.pkl", 'rb'))
+locations = sorted(data['location'].unique())
+#standard_to = StandardScaler()
+@app.route('/')
+def index():
+    
+    return render_template('index.html', locations=locations)
+    #return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    location = request.form.get('location')
+    bhk = request.form.get('bhk')
+    bath = request.form.get('bath')
+    sqft = request.form.get('total_sqft')
+
+    print(location, bhk, bath, sqft)
+    input = pd.DataFrame([[location, sqft, bath, bhk]], columns=['location', 'total_sqft', 'bath', 'bhk'])
+    prediction = pipe.predict(input)[0] * 1e5
+
+    return str(np.round(prediction, 2))
+
+
+if __name__ == "__main__":
+    app.run()
